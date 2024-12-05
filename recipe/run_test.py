bf8353f65fb3c61f2fd1de0630b4914a93b29756
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-12-05 08:31:12 -0600

    move tests to py for linter

---
+++ recipe/run_test.py
@@ -0,0 +1,24 @@
+import sys
+from subprocess import call
+
+FAIL_UNDER = 91
+
+# added on https://github.com/conda-forge/openapi-core-feedstock/pull/22
+SKIPS = [
+    "test_request_response_error",
+    "test_spec_invalid",
+    "test_spec_not_detected",
+    "test_webhook_request_validator_not_found",
+    "test_spec_not_detected",
+    "test_spec_not_supported",
+    "test_webhook_response_validator_not_found",
+]
+
+COV = ["coverage"]
+K = [f"""not ({" or ".join(SKIPS)})"""]
+PYTEST = ["pytest", "-vv", "--tb=long", "--color=yes", "-k", *K]
+RUN = ["run", "--source", "openapi_core", "--branch", "-m", *PYTEST]
+REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
+
+if __name__ == "__main__":
+    sys.exit(max([call([*COV, *RUN]), call([*COV, *REPORT])]))
